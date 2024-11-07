55f2c087dd579d63ce79e8628e707edabb996a9a
leehjhjhj <tsukiakarii@naver.com>
2024-11-07 21:22:07 +0900

    feat: forbidden 예외 추가

--- functions/common/exceptions.py
+++ functions/common/exceptions.py
@@ -2,3 +2,8 @@ class NotFoundException(Exception):
     def __init__(self, message="Url not found"):
         self.message = message
         super().__init__(self.message)
+
+class ForbiddenException(Exception):
+    def __init__(self, message="Forbidden"):
+        self.message = message
+        super().__init__(self.message)
