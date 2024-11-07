34b5d348c80c6ef6311578427c6275d52ae6ac43
leehjhjhj <tsukiakarii@naver.com>
2024-11-07 20:51:14 +0900

    refactor: 예외처리 port로 이동

--- functions/redirect/service.py
+++ functions/redirect/service.py
@@ -2,7 +2,6 @@ from functions.redirect.adapter import RedirectPort
 from functions.common.response import LambdaResponse
 from functions.redirect.schema import RedirectRequest
 
-
 class RedirectService:
     def __init__(self, adapter: RedirectPort):
         self._adapter = adapter
@@ -10,12 +9,6 @@ class RedirectService:
     def connect_url(self, request: RedirectRequest) -> dict:
         hash_value = request.hash_value
         result = self._adapter.find(hash_value)
-        if not result.origin_url:
-            return LambdaResponse(
-                status_code=404,
-                body={'error': 'URL not found'}
-            ).to_dict()
-
         return LambdaResponse(
             status_code=302,
             headers={
