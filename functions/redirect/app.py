7dd515c6345f50b2a1ae211cec8407ccfee94078
leehjhjhj <tsukiakarii@naver.com>
2024-11-07 20:36:33 +0900

    feat: Rediret Controller 추가

--- functions/redirect/app.py
+++ functions/redirect/app.py
@@ -1,3 +1,24 @@
+from functions.redirect.container import RedirectContainer
+from functions.redirect.schema import RedirectRequest
+from functions.common.response import LambdaResponse
 
 def lambda_handler(event, context):
-    pass
+    try:
+        container = RedirectContainer.get_instance()
+        hash_value = event['pathParameters']['hash']
+        request = RedirectRequest(hash_value=hash_value)
+        response = container.service.connect_url(request)
+        return response
+    except Exception as e:
+        print(e)
+        return LambdaResponse(
+            status_code=500,
+            message="Internal server error"
+        ).to_dict()
+
+event = {
+    "pathParameters": {
+        "hash": "test"
+    }
+}
+print(lambda_handler(event, ""))
