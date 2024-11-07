f0c5f246b89e15b6698bdf6901305bb13e074e50
leehjhjhj <tsukiakarii@naver.com>
2024-11-07 20:35:01 +0900

    chore: redirect 컨테이너 추가

---
+++ functions/redirect/container.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+from functions.redirect.service import RedirectService
+from functions.redirect.adapter import UrlDynamoDBPort
+from functions.redirect.environments import env
+from typing import Optional
+
+class RedirectContainer:
+    _instance: Optional[RedirectContainer] = None
+    
+    def __init__(self):
+        self._adapter = UrlDynamoDBPort(table_name=env.TABLE_NAME, region=env.REGION)
+        self.service = RedirectService(self._adapter)
+    
+    @classmethod
+    def get_instance(cls) -> RedirectContainer:
+        if cls._instance is None:
+            cls._instance = cls()
+        return cls._instance
