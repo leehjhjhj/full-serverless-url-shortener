95740aac3fac18cd1c49fdd43b59f668cc89df7b
leehjhjhj <tsukiakarii@naver.com>
2024-11-07 20:51:02 +0900

    refactor: NotFoundException 발생시 404 반환

--- functions/redirect/app.py
+++ functions/redirect/app.py
@@ -1,6 +1,7 @@
 from functions.redirect.container import RedirectContainer
 from functions.redirect.schema import RedirectRequest
 from functions.common.response import LambdaResponse
+from functions.common.exceptions import NotFoundException
 
 def lambda_handler(event, context):
     try:
@@ -9,6 +10,11 @@ def lambda_handler(event, context):
         request = RedirectRequest(hash_value=hash_value)
         response = container.service.connect_url(request)
         return response
+    except NotFoundException as e:
+        return LambdaResponse(
+                status_code=404,
+                body={'error': e.message}
+            ).to_dict()
     except Exception as e:
         print(e)
         return LambdaResponse(
@@ -18,7 +24,7 @@ def lambda_handler(event, context):
 
 event = {
     "pathParameters": {
-        "hash": "test"
+        "hash": "1"
     }
 }
 print(lambda_handler(event, ""))
