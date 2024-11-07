37237b470283d6eda28ef8350dcc5711bb34b984
leehjhjhj <tsukiakarii@naver.com>
2024-11-07 22:19:34 +0900

    refactor: alias, serialization alias 추가

--- functions/redirect/schema.py
+++ functions/redirect/schema.py
@@ -1,8 +1,11 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class RedirectRequest(BaseModel):
     hash_value: str
 
-class OriginUrlData(BaseModel):
-    origin_url: str
+class DataDto(BaseModel):
+    hash_value: str = Field(..., serialization_alias='hash', alias='hash')
+    id: int
+    origin_url: str = Field(..., serialization_alias='ou', alias='ou')
     on: bool
+    count: int = Field(..., serialization_alias='ct', alias='ct')
