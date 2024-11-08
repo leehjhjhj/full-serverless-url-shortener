1c4837d45b764188f69ba103cff526bc9a170169
leehjhjhj <tsukiakarii@naver.com>
2024-11-08 22:41:26 +0900

    chore: ConfigDict에 populate_by_name 추가

--- functions/common/common_schema.py
+++ functions/common/common_schema.py
@@ -1,7 +1,8 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Optional
 
 class UrlSchema(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
     hash_value: str = Field(..., serialization_alias='hash', alias='hash')
     origin_url: str = Field(..., serialization_alias='ou', alias='ou')
     # user_id: Optional[str] = Field(default=None, serialization_alias='ui', alias='ui')
