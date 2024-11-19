41d91bb8ec7a359a192eec6283460395af0086eb
leehjhjhj <tsukiakarii@naver.com>
2024-11-19 15:32:37 +0900

    feat: CRUD DTO 추가

---
+++ functions/crud/schema.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Optional
+
+
+class UrlDto(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+    hash_value: str = Field(..., serialization_alias='hash', alias='hash')
+    origin_url: str = Field(..., serialization_alias='originUrl', alias='ou')
+    created_at: int = Field(..., serialization_alias='createdAt', alias='ca')
+    count: int = Field(default=0, serialization_alias='count', alias='ct')
+    on: bool = Field(default=True)
+    title: Optional[str] = Field(default=None, serialization_alias='title', alias='ti')
+
+class SearchAllResult(BaseModel):
+    result: list[UrlDto]
+    last_key: Optional[dict] = Field(..., serialization_alias='lastKey')
+
+class SearchResult(BaseModel):
+    result: list[UrlDto]
