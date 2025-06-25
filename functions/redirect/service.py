b22154c5c4a9b072911371d8d7348c495e2120a6
hyunje <hyunje@hyunjeui-MacBookPro.local>
2025-06-25 11:47:48 +0900

    feat: booth register url 변경

--- functions/redirect/service.py
+++ functions/redirect/service.py
@@ -47,7 +47,7 @@ class RedirectService:
             "d": f"/demand/view/{unique_id}",
             "r": f"/paper/view/{unique_id}",
             "l": f"/event/@{event_url}/timeline/{unique_id}",
-            "b": f"/event/@{event_url}/booth/{unique_id}"
+            "b": f"/event/@{event_url}/booth/ready/{unique_id}"
         }
         return mapping.get(type)
     
